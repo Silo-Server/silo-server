9aac775236bff70ad2cd661a2db53b47aa51b586
Silo Server Developer <warmasterx555@gmail.com>
2026-06-10 20:51:48 +0200

    feat(scanner): manga filename index/volume parser

---
+++ internal/scanner/manga_parse_test.go
@@ -0,0 +1,30 @@
+package scanner
+
+import "testing"
+
+func TestParseMangaIndex(t *testing.T) {
+	cases := []struct {
+		name    string
+		file    string
+		wantVol string
+		wantIdx float64
+		wantHas bool
+	}{
+		{"bare chapter", "One-Punch Man 178 (2023) (Digital) (LuCaZ)", "", 178, true},
+		{"volume", "Bakuman v13 (2012) (Digital) (aKraa)", "v13", 13, true},
+		{"chapter c-prefix", "Dead Mount Death Play c128 (2025) (Digital) (UP!) (Oak)", "", 128, true},
+		{"vol-year issue", "Berserk Vol.2003 #04 (July, 2004)", "Vol.2003", 4, true},
+		{"vol-year no issue", "Berserk Vol.2003 (2004)", "", 0, false},
+		{"decimal chapter", "Kindergarten WARS 109.1 (2025) (Digital) (Rillant)", "", 109.1, true},
+		{"subtitle then volume", "The Ancient Magus' Bride - Wizard's Blue v04 (2022) (Digital)", "v04", 4, true},
+		{"no number", "Some Oneshot (2020) (Digital) (grp)", "", 0, false},
+	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			vol, idx, has := parseMangaIndex(tc.file)
+			if vol != tc.wantVol || has != tc.wantHas || idx != tc.wantIdx {
+				t.Fatalf("parseMangaIndex(%q) = (%q,%v,%v), want (%q,%v,%v)", tc.file, vol, idx, has, tc.wantVol, tc.wantIdx, tc.wantHas)
+			}
+		})
+	}
+}
